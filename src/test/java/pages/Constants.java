3cc9655854ebc4936fdad47b2b12c42ae4dceea8
sheetal singh <sheetalsingh@xebia.com>
2017-05-16 09:41:57 +0530

    updated pom version

--- src/test/java/pages/Constants.java
+++ src/test/java/pages/Constants.java
@@ -5,8 +5,8 @@ import org.openqa.selenium.By;
 public class Constants {
 
 	// YAHOO HOME PAGE
-	public static By YAHOOHOME_TEXTBOX = By.id("UHSearchBox");
-	public static By YAHOOHOME_SEARCH_BUTTON = By.id("UHSearchWeb");
+	public static By YAHOOHOME_TEXTBOX = By.xpath("//input[@id='uh-search-box']");
+	public static By YAHOOHOME_SEARCH_BUTTON = By.id("uh-search-button");
 
 	// YAHOO SEARCH PAGE
 	public static By YAHOOSEARCH_SELENIUM_HQ = By.xpath("//a[contains(text(),'Selenium - Official Site')]");
