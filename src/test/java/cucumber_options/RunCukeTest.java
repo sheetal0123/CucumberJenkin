f2f4823fff2fe7205988dae8ddb8761d93d512c0
sheetal <sheetal0123@gmail.com>
2019-02-25 21:53:09 +0530

    modified feature folder location

--- src/test/java/cucumber_options/RunCukeTest.java
+++ src/test/java/cucumber_options/RunCukeTest.java
@@ -1,21 +1,22 @@
 package cucumber_options;
 
 import org.junit.runner.RunWith;
-
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+
 
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		
-		features = "src/test/java/features", 
- glue = "step_definitions", 
- monochrome = true, dryRun = false,
+		features = "src/test/resources/features", 
+		glue = "step_definitions", 
+		monochrome = true, 
+		dryRun = false,
 		plugin = {
- "pretty",
-		"html:target/htmlreport",
-		}
+				"pretty",
+				"html:target/htmlreport",
+			}
 		)
 public class RunCukeTest {
 
